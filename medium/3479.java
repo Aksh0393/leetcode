7a5d3ddf41da7a9c9d052c2972c1bcf24d8b1030
akshitgg9 <akshit.thakur@xperi.com>
2025-08-06 15:07:37 +0530

    updated 3479

--- medium/3479.java
+++ medium/3479.java
@@ -1,61 +1,68 @@
+import java.util.*;
+
 class Solution {
 
     static class SegmentTree {
-
-        int tree[];
+        int[] tree;
         int size;
 
-        SegmentTree(int a[]) {
-            this.size = a.length;
-            this.tree = new int[4 * size];
-
+        SegmentTree(int[] arr) {
+            size = arr.length;
+            tree = new int[4 * size];
+            build(arr, 0, 0, size - 1);
         }
 
-        void build(int a[], int node, int l, int r) {
-            if (l == r)
-                tree[node] = a[l];
-            else {
-                int mid = l + (r - l) >> 1;
-                build(a, 2 * node + 1, l, mid);
-                build(a, 2 * node + 2, mid + 1, r);
+        private void build(int[] arr, int node, int l, int r) {
+            if (l == r) {
+                tree[node] = arr[l];
+            } else {
+                int mid = l + ((r - l) >> 1);
+                build(arr, 2 * node + 1, l, mid);         // left child
+                build(arr, 2 * node + 2, mid + 1, r);     // right child
                 tree[node] = Math.max(tree[2 * node + 1], tree[2 * node + 2]);
             }
         }
 
+        // Find the leftmost index with capacity >= k
         int search(int node, int l, int r, int k) {
-            if (tree[node] < k)
-                return -1;
-
+            if (tree[node] < k) return -1;
             if (l == r) {
-                tree[node] = -1;
+                tree[node] = -1; // mark as used
                 return l;
             }
 
-            int mid = l + (r - l) >> 1;
-            int pos = -1;
-            if (tree[2 * node + 1] >= k)
+            int mid = l + ((r - l) >> 1);
+            int pos;
+            if (tree[2 * node + 1] >= k) {
                 pos = search(2 * node + 1, l, mid, k);
-            else
+            } else {
                 pos = search(2 * node + 2, mid + 1, r, k);
+            }
 
+            // Update current node after change
             tree[node] = Math.max(tree[2 * node + 1], tree[2 * node + 2]);
             return pos;
         }
-
     }
 
-    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+    public static int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        SegmentTree segTree = new SegmentTree(baskets);
 
-        SegmentTree segmentTree = new SegmentTree(baskets);
-        int count =0;
-
-        for(int fruit:fruits){
-            int placedIndex = segmentTree.search(0,0,segmentTree.size -1,fruit);
-            if (placedIndex ==-1)
-                count++;
-
+        int unplaced = 0;
+        for (int fruit : fruits) {
+            int placedIndex = segTree.search(0, 0, n - 1, fruit);
+            if (placedIndex == -1) {
+                unplaced++;
+            }
         }
+        return unplaced;
+    }
 
-        return count;
+    public static void main(String[] args) {
+        int[] fruits = {4, 2, 3};
+        int[] baskets = {3, 5, 2};
+
+        System.out.println(numOfUnplacedFruits(fruits, baskets)); // Output: 1
     }
 }
