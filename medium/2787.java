969cb930bae4bd9788cf80e38279fec8c6a6e817
akshitgg9 <akshit.thakur@xperi.com>
2025-08-13 21:10:44 +0530

    daily soln

--- medium/2787.java
+++ medium/2787.java
@@ -1,37 +1,39 @@
+import java.util.Arrays;
+
 class Solution {
-    private final int M = 1000000007; // 1e9 + 7
-    private int[][] t = new int[301][301];
-
-    private int solve(int n, int num, int x) {
-        if (n == 0) {
-            return 1;
-        }
-        if (n < 0) {
-            return 0;
-        }
-
-        int currPowerValue = (int) Math.pow(num, x);
-        if (currPowerValue > n) {
-            return 0;
-        }
-
-        if (t[n][num] != -1) {
-            return t[n][num];
-        }
-
-        int take = solve(n - currPowerValue, num + 1, x);
-        int skip = solve(n, num + 1, x);
-
-        return t[n][num] = (take + skip) % M;
-    }
+    int mod = 1_000_000_007;
+    int dp[][];
 
     public int numberOfWays(int n, int x) {
-        // Fill memoization array with -1
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                t[i][j] = -1;
-            }
-        }
+        dp = new int[n + 1][n + 1];
+        for (int i[] : dp)
+            Arrays.fill(i, -1);
         return solve(n, 1, x);
+    }
+
+    private int solve(int n, int num, int x) {
+        if (n == 0)
+            return 1;
+
+        if (n < 0)
+            return 0;
+
+        if (num > n)
+            return 0;
+
+        if (dp[n][num] != -1)
+            return dp[n][num];
+
+        int powerVal = (int) Math.pow(num, x);
+
+        int take = solve(n - powerVal, num + 1, x);
+        int notTake = solve(n, num + 1, x);
+
+        return dp[n][num] = (take + notTake) % mod;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.numberOfWays(3, 1);
     }
 }
