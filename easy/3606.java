b528acb7f842a81a41c0d58a67258d87c546d33d
akshitgg9 <akshit.thakur@xperi.com>
2025-12-13 22:24:26 +0530

    updated 3606

--- easy/3606.java
+++ easy/3606.java
@@ -1,76 +1,48 @@
 import java.util.*;
 
 class Solution {
-    public List<String> validateCoupons(
-        List<String> codes,
-        List<String> businessLines,
-        List<Boolean> isActives
-    ) {
-        int n = codes.size();
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List<String>res = new ArrayList();
 
-        // Mapping from business line name to its required sort order
-        Map<String, Integer> businessLineSortOrder = new HashMap<>();
-        businessLineSortOrder.put("electronics", 0);
-        businessLineSortOrder.put("grocery", 1);
-        businessLineSortOrder.put("pharmacy", 2);
-        businessLineSortOrder.put("restaurant", 3);
+        List<String>e = new ArrayList<>();
+        List<String>g = new ArrayList();
+        List<String>p = new ArrayList();
+        List<String>r = new ArrayList();
+        
+        for(int i=0;i<isActive.length;i++){
+            if(isActive[i]){
+                if(businessLine[i].equals("electronics") 
+                    || businessLine[i].equals("grocery")
+                  || businessLine[i].equals("pharmacy")
+                  || businessLine[i].equals("restaurant")){
+                    if(code[i].length() == 0)continue;
+                    boolean found = true;
+                    for(int j=0;j<code[i].length();j++){
+                        char ch = code[i].charAt(j);
+                        if (!Character.isLetterOrDigit(ch) && ch != '_') {
+                            found = false;
+                            break;
+                        }
+                    }
 
-        // Store each valid coupon with info needed for sorting
-        // Each CouponInfo stores (sortIndex, code)
-        List<CouponInfo> sortableCoupons = new ArrayList<>();
-
-        for (int i = 0; i < n; ++i) {
-            // Check coupon is currently active
-            if (!isActives.get(i)) continue;
-
-            // Check if business line is valid
-            if (!businessLineSortOrder.containsKey(businessLines.get(i))) continue;
-
-            // Check if code is non-empty and only alphanumeric or underscore
-            String code = codes.get(i);
-            if (code.isEmpty()) continue;
-            boolean isCodeValid = true;
-            for (char c : code.toCharArray()) {
-                if (!Character.isLetterOrDigit(c) && c != '_') {
-                    isCodeValid = false;
-                    break;
-                }
+                    if(found){
+                        if(businessLine[i].startsWith("e"))e.add(code[i]);
+                        if(businessLine[i].startsWith("g"))g.add(code[i]);
+                        if(businessLine[i].startsWith("p"))p.add(code[i]);
+                        if(businessLine[i].startsWith("r"))r.add(code[i]);
+                    }
+                  }
             }
-            if (!isCodeValid) continue;
-
-            // Everything is valid, collect coupon for sorting
-            int sortIndex = businessLineSortOrder.get(businessLines.get(i));
-            sortableCoupons.add(new CouponInfo(sortIndex, code));
         }
-
-        // Sort by business line order, then code alphabetically
-        Collections.sort(sortableCoupons);
-
-        // Gather the sorted coupon codes
-        List<String> sortedValidCodes = new ArrayList<>();
-        for (CouponInfo coupon : sortableCoupons) {
-            sortedValidCodes.add(coupon.code);
-        }
-
-        return sortedValidCodes;
-    }
-
-    // Helper class for sorting
-    static class CouponInfo implements Comparable<CouponInfo> {
-        int sortIndex;
-        String code;
-
-        CouponInfo(int sortIndex, String code) {
-            this.sortIndex = sortIndex;
-            this.code = code;
-        }
-
-        @Override
-        public int compareTo(CouponInfo other) {
-            if (this.sortIndex != other.sortIndex) {
-                return Integer.compare(this.sortIndex, other.sortIndex);
-            }
-            return this.code.compareTo(other.code);
-        }
+        Collections.sort(e);
+        Collections.sort(g);
+        Collections.sort(p);
+        Collections.sort(r);
+        res.addAll(e);
+        res.addAll(g);
+        res.addAll(p);
+        res.addAll(r);
+        
+        return res;
     }
 }
