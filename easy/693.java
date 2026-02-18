eb49b1cefedd0fe018027a25ef796be4267b79e8
akshitgg9 <akshit.thakur@xperi.com>
2026-02-18 15:55:59 +0530

    updated 693

--- easy/693.java
+++ easy/693.java
@@ -1,11 +1,6 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
-        String bits = Integer.toBinaryString(n);
-        for (int i = 0; i < bits.length() - 1; i++) {
-            if (bits.charAt(i) == bits.charAt(i+1)) {
-                return false;
-            }
-        }
-        return true;
+        int xor = n ^ (n >> 1); // XOR of n and n shifted right by 1 will give us a number that has all bits set to 1 if n has alternating bits. For example, if n is 5 (binary 101), then n >> 1 is 2 (binary 10), and n ^ (n >> 1) will be 7 (binary 111). If n is 7 (binary 111), then n >> 1 is 3 (binary 11), and n ^ (n >> 1) will be 4 (binary 100), which does not have all bits set to 1.
+        return (xor & (xor + 1)) == 0; // If xor has all bits set to 1, then xor + 1 will have only one bit set to 1 (the next power of 2), and the bitwise AND of xor and xor + 1 will be 0. If xor does not have all bits set to 1, then the bitwise AND will not be 0.
     }
 }
